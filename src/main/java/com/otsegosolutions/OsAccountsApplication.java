41003531463a524a12e1d20714f8c24d1bf5e5b9
Randy Carlson <rcarlson@otsegosolutions.com>
2016-03-10 21:24:57 -0600

    Rando: intial check in

---
+++ src/main/java/com/otsegosolutions/OsAccountsApplication.java
@@ -0,0 +1,12 @@
+package com.otsegosolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsAccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsAccountsApplication.class, args);
+	}
+}
