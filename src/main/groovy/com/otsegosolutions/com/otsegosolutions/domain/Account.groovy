8db6b9e799718ad8454c8e2b70ad00c018e27eb1
Randy Carlson <rcarlson@otsegosolutions.com>
2016-03-13 13:50:21 -0500

    Rando: gradle... ahhhhh

--- src/main/groovy/com/otsegosolutions/com/otsegosolutions/domain/Account.groovy
+++ src/main/groovy/com/otsegosolutions/com/otsegosolutions/domain/Account.groovy
@@ -1,12 +1,14 @@
 package com.otsegosolutions.com.otsegosolutions.domain
 
+//import org.springframework.data.annotation.Id
+
 class Account {
 
 //    @Id
-//    Long id
-//    String accountDesc
-//    String userId
-//    String passwordHint
-//    String pinHint
-//    String url
+    Long id
+    String accountDesc
+    String userId
+    String passwordHint
+    String pinHint
+    String url
 }
