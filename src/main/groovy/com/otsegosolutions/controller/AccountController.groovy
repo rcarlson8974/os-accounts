5d3317c295a29631481d2b112d653cfe082e714d
Randy Carlson <rcarlson@otsegosolutions.com>
2016-03-13 11:55:23 -0500

    Rando: WIP

--- src/main/groovy/com/otsegosolutions/controller/AccountController.groovy
+++ src/main/groovy/com/otsegosolutions/controller/AccountController.groovy
@@ -1,5 +1,21 @@
 package com.otsegosolutions.controller
 
+import com.otsegosolutions.service.AccountService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
+
+@RestController
+@EnableAutoConfiguration
 class AccountController {
+
+    @Autowired
+    AccountService service
+
+    @RequestMapping("/")
+    String home() {
+        return service.test()
+    }
 }
---
+++ src/main/groovy/com/otsegosolutions/service/AccountService.groovy
@@ -0,0 +1,11 @@
+package com.otsegosolutions.service
+
+import org.springframework.stereotype.Service
+
+@Service
+class AccountService {
+
+    String test()  {
+        return "foo"
+    }
+}
